9f23cbd096dec839d2a101039069f1afcb3d551b
Bogdan <seclan@mail.ru>
2016-12-09 02:13:42 +0700

    fix conf error

--- server/rabbit/config.js
+++ server/rabbit/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export const connectionData = {
+export default {
   name: process.env.CONF_RABBIT_LOGIN || 'guest',
   password: process.env.CONF_RABBIT_PASSWORD || 'guest',
   host: process.env.CONF_RABBIT_HOST || 'localhost',
